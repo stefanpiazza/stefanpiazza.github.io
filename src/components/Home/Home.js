80eb7c787913104609144db0e641ffd48553416b
Stefan Piazza <stefanpiazza@gmail.com>
2017-09-23 16:12:53 +0100

    UPDATE: Updates to styles and layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,21 +10,24 @@ class Home extends React.Component {
         return (
             <div className="content">
                 <div className="content__header">
-                    <h1 className="content__title">
+                    <h1 className="content__title content__reveal content__reveal--left">
                         <div className="content__element">Stefan Piazza</div>
-                    </h1>
-                    <p className="content__subtitle">
+                        <div className="content__revealer content__revealer--primary"></div>
+                    </h1><br></br>
+                    <p className="content__subtitle content__reveal content__reveal--left content__reveal--delay-200">
                         <div className="content__element">Front End Developer</div>
-                    </p>
-                    <p className="content__bio">
-                        <div className="content__element">Energetic and passionate front end developer with a BSc in Computer Science from the University of Leeds. Loves experimenting and hacking together new ideas. Always with a side project on the go and a passion for collaborating with people to make something cool.</div>
+                        <div className="content__revealer content__revealer--primary"></div>
                     </p>
                 </div>
                 <div className="content__footer">
-                    <div className="content__image">
-                        <img className="content__element"></img>
-                        <div className="content__revealer"></div>
+                    <div className="content__image content__reveal content__reveal--right content__reveal--delay-600">
+                        <div className="content__element"></div>
+                        <div className="content__revealer content__revealer--secondary"></div>
                     </div>
+                    <p className="content__bio content__reveal content__reveal--left content__reveal--delay-400">
+                        <div className="content__element">Github, CodePen and LinkedIn</div>
+                        <div className="content__revealer content__revealer--primary"></div>
+                    </p>
                 </div>
             </div>
         );
