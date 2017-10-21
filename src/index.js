08ba9b69edc06ae679a4d55519fa5afbd4dddb59
Stefan Piazza <stefanpiazza@gmail.com>
2017-10-21 16:04:31 +0100

    UPDATE: Remove old files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var styles = require('./index.scss');
-
-var monkey320 = require('./monkey-320.jpg');
-var monkey729 = require('./monkey-729.jpg');
-var monkey1006 = require('./monkey-1006.jpg');
-var monkey1224 = require('./monkey-1224.jpg');
-var monkey1440 = require('./monkey-1440.jpg');
